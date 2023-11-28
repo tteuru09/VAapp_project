feaebb8dc6570c30cc038f40c607699d20a3b0ae
tteuru09 <toamiriurateururai@gmail.com>
2023-11-27 14:35:11 -1000

    Seeder_v0.1

--- Dev/VAapp/database/seeders/DatabaseSeeder.php
+++ Dev/VAapp/database/seeders/DatabaseSeeder.php
@@ -12,11 +12,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory(2)->create([
+            'status' => 'trainer'
+        ]);
+        \App\Models\User::factory(15)->create([
+            'status' => 'rower'
+        ]);
+        \App\Models\User::factory()->create([
+            'status' => 'admin'
+        ]);
+        \App\Models\Canoe::factory()->create([
+            'numberOfPlace' => 6
+        ]);
+        \App\Models\Canoe::factory()->create([
+            'numberOfPlace' => 3
+        ]);
+        \App\Models\Canoe::factory(2)->create([
+            'numberOfPlace' => 1
+        ]);
     }
 }
