c23f0514391f01af366fd2a749e92aedd4ace4f9
tteuru09 <toamiriurateururai@gmail.com>
2023-12-15 13:52:22 -1000

    AppJs_v0.5

--- Dev/VAapp/resources/js/app.js
+++ Dev/VAapp/resources/js/app.js
@@ -10,62 +10,104 @@ import { Carousel, Collapse, Input, Datepicker, Select, Timepicker, Modal, Rippl
 initTE({ Carousel, Collapse , Input, Modal, Ripple, Select }); // set second parameter to true if you want to use a debugger
 
 //Delete modal
-const modalDelete = document.getElementById('modalDelete');
-modalDelete.addEventListener('show.te.modal', (e) => {
+const modalDeleteSlot = document.getElementById('modalDeleteSlot');
+
+if (modalDeleteSlot != null){
+  modalDeleteSlot.addEventListener('show.te.modal', (e) => {
   
-  const button = e.relatedTarget;
+    const button = e.relatedTarget;
+    
+    const slot = button.getAttribute('data-te-whatever');
+    
+    const inputToChange = modalDeleteSlot.querySelector("#ModalFooter input[name='slot_id']");
+    inputToChange.value = slot;
+  })
+}
+
+const modalDeleteCanoe = document.getElementById('modalDeleteCanoe');
+
+if (modalDeleteCanoe != null){
+  modalDeleteCanoe.addEventListener('show.te.modal', (e) => {
   
-  const slot = button.getAttribute('data-te-whatever');
-  
-  const inputToChange = modalDelete.querySelector("#ModalFooter input[name='slot_id']");
-  inputToChange.value = slot;
-})
+    const button = e.relatedTarget;
+    
+    const canoe = button.getAttribute('data-te-whatever');
+    
+    const inputToChange = modalDeleteCanoe.querySelector("#ModalFooter input[name='canoe_id']");
+    inputToChange.value = canoe;
+  })
+}
+
 
 //Edit modal 
-const modalEdit = document.getElementById('modalEdit');
-modalEdit.addEventListener('show.te.modal', (e) => {
-  const button = e.relatedTarget;
-
-  //Constantes
-  const params = JSON.parse(button.getAttribute('data-te-whatever'));
-  console.log(params)
-  const date = params.slot.date.split("-");
-  const start_time = params.slot.start_time.split(":");
-  const end_time = params.slot.end_time.split(":");
-
-  // Recup les inputs
-  const slotID = modalEdit.querySelector("#ModalEditBody input[name='slot_id']");
-  const dateEdit = modalEdit.querySelector("#ModalEditBody input[name='dateEdit']");
-  const timeStartEdit = modalEdit.querySelector("#ModalEditBody input[name='timeStartEdit']");
-  const timeEndEdit = modalEdit.querySelector("#ModalEditBody input[name='timeEndEdit']");
-  const selectCanoes = modalEdit.querySelector("#ModalEditBody select[name='canoes[]']");
-  const selectRowers = modalEdit.querySelector("#ModalEditBody select[name='rowers[]']");
+const modalEditSlot = document.getElementById('modalEditSlot');
+if(modalEditSlot != null){
+    modalEditSlot.addEventListener('show.te.modal', (e) => {
+    const button = e.relatedTarget;
   
-  const canoesLength = selectCanoes.options.length
-  const rowersLength = selectRowers.options.length
+    //Constantes
+    const params = JSON.parse(button.getAttribute('data-te-whatever'));
+    const date = params.slot.date.split("-");
+    const start_time = params.slot.start_time.split(":");
+    const end_time = params.slot.end_time.split(":");
+  
+    // Recup les inputs
+    const slotID = modalEditSlot.querySelector("#ModalEditBody input[name='slot_id']");
+    const dateEdit = modalEditSlot.querySelector("#ModalEditBody input[name='dateEdit']");
+    const timeStartEdit = modalEditSlot.querySelector("#ModalEditBody input[name='timeStartEdit']");
+    const timeEndEdit = modalEditSlot.querySelector("#ModalEditBody input[name='timeEndEdit']");
+    const selectCanoes = modalEditSlot.querySelector("#ModalEditBody select[name='canoes[]']");
+    const selectRowers = modalEditSlot.querySelector("#ModalEditBody select[name='rowers[]']");
+    
+    const canoesLength = selectCanoes.options.length
+    const rowersLength = selectRowers.options.length
+  
+    for (let i = 0; i < canoesLength; i++) {
+      selectCanoes.options[i].removeAttribute('selected')
+    }
+  
+    for (let i = 0; i < rowersLength; i++) {
+      selectRowers.options[i].removeAttribute('selected')
+    }
+  
+    Object.keys(params.canoes).forEach(key => {
+      selectCanoes.options.namedItem(params.canoes[key].ref_canoe).setAttribute("selected",'')
+    });
+  
+    Object.keys(params.rowers).forEach(key => {
+      selectRowers.options.namedItem(params.rowers[key].ref_rower).setAttribute("selected",'')
+    });
+  
+    // Change value of inputs
+    slotID.value = params.slot.id;
+    dateEdit.value = date[2] + "-" + date[1] + "-" + date[0];
+    timeStartEdit.value = start_time[0] + ":" + start_time[1];
+    timeEndEdit.value = end_time[0] + ":" + end_time[1];
+  })
+}
 
-  for (let i = 0; i < canoesLength; i++) {
-    selectCanoes.options[i].removeAttribute('selected')
-  }
+const modalEditCanoe = document.getElementById('modalEditCanoe');
+if(modalEditCanoe != null){
+    modalEditCanoe.addEventListener('show.te.modal', (e) => {
+    const button = e.relatedTarget;
+  
+    //Constantes
+    const params = JSON.parse(button.getAttribute('data-te-whatever'));
+    const idInput = modalEditCanoe.querySelector("#ModalEditBody input[name='canoe_id']");
+    const nameInput = modalEditCanoe.querySelector("#ModalEditBody input[name='vaa_name_edit']");
+    const placeSelect = modalEditCanoe.querySelector("#ModalEditBody select[name='numberOfPlaceEdit']");
+    
+    for (let i = 0; i < 3; i++) {
+      placeSelect.options[i].removeAttribute('selected')
+    }
 
-  for (let i = 0; i < rowersLength; i++) {
-    selectRowers.options[i].removeAttribute('selected')
-  }
+    // Change value of inputs
+    idInput.value = params.id;
+    nameInput.value = params.name;
+    placeSelect.options.namedItem(params.numberOfPlace).setAttribute("selected",'');
+  })
+}
 
-  Object.keys(params.canoes).forEach(key => {
-    selectCanoes.options.namedItem(params.canoes[key].ref_canoe).setAttribute("selected",'')
-  });
-
-  Object.keys(params.rowers).forEach(key => {
-    selectRowers.options.namedItem(params.rowers[key].ref_rower).setAttribute("selected",'')
-  });
-
-  // Change value of inputs
-  slotID.value = params.slot.id;
-  dateEdit.value = date[2] + "-" + date[1] + "-" + date[0];
-  timeStartEdit.value = start_time[0] + ":" + start_time[1];
-  timeEndEdit.value = end_time[0] + ":" + end_time[1];
-})
 
 
 //New slot date time
